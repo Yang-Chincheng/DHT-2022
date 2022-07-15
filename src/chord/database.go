b8c539d0a8257f02d5743253fdf021b7add7c58e
Modem514 <indigea514@gmail.com>
2022-07-14 20:23:22 -0700

    refactor(database): interface adjusted

--- src/chord/database.go
+++ src/chord/database.go
@@ -11,17 +11,27 @@ type databaseNode struct {
 	backup     map[string]string
 }
 
+func (n *databaseNode) storeInit() {
+	n.data = make(map[string]string)
+	n.backup = make(map[string]string)
+}
+
+func (n *databaseNode) storeReset() {
+	n.data = make(map[string]string)
+	n.backup = make(map[string]string)
+}
+
 func (n *databaseNode) PutData(p dataPair, _ interface{}) error {
 	n.dataLock.Lock()
 	defer n.dataLock.Unlock()
-	n.data[p.k] = p.k
+	n.data[p.k] = p.v
 	return nil
 }
 
 func (n *databaseNode) PutBackup(p dataPair, _ interface{}) error {
 	n.backupLock.Lock()
 	defer n.backupLock.Unlock()
-	n.backup[p.k] = p.k
+	n.backup[p.k] = p.v
 	return nil
 }
 
@@ -39,14 +49,30 @@ func (n *databaseNode) GetBackup(k string, v *string) error {
 	return nil
 }
 
-func (n *databaseNode) DelteData(k string, _ interface{}) error {
+func (n *databaseNode) SetData(mp map[string]string, _ interface{}) error {
+	n.data = make(map[string]string)
+	for k, v := range mp {
+		n.data[k] = v
+	}
+	return nil
+}
+
+func (n *databaseNode) SetBackup(mp map[string]string, _ interface{}) error {
+	n.backup = make(map[string]string)
+	for k, v := range mp {
+		n.backup[k] = v
+	}
+	return nil
+}
+
+func (n *databaseNode) DeleteData(k string, _ interface{}) error {
 	n.dataLock.Lock()
 	defer n.dataLock.Unlock()
 	delete(n.data, k)
 	return nil
 }
 
-func (n *databaseNode) DelteBackup(k string, _ interface{}) error {
+func (n *databaseNode) DeleteBackup(k string, _ interface{}) error {
 	n.backupLock.Lock()
 	defer n.backupLock.Unlock()
 	delete(n.backup, k)
@@ -91,6 +117,20 @@ func (n *databaseNode) FilterBackup(filter func(string) bool, res map[string]str
 			delete(n.backup, k)
 			res[k] = v
 		}
+	}
+	return nil
+}
+
+func (n *databaseNode) CopyData(_ interface{}, mp map[string]string) error {
+	for k, v := range n.data {
+		mp[k] = v
+	}
+	return nil
+}
+
+func (n *databaseNode) CopyBackup(_ interface{}, mp map[string]string) error {
+	for k, v := range n.backup {
+		mp[k] = v
 	}
 	return nil
 }
