0ce383f1a254ea03c412b54fab7d5cd896d1ba6c
Modem514 <indigea514@gmail.com>
2022-07-14 20:26:50 -0700

    feat(chord): chord protocol interface added

--- src/main/userdef.go
+++ src/main/userdef.go
@@ -1,11 +1,16 @@
 package main
 
+import (
+	"DHT-2022/src/chord"
+	"fmt"
+)
+
 /* In this file, you should implement function "NewNode" and
  * a struct which implements the interface "dhtNode".
  */
 
 func NewNode(port int) dhtNode {
-	// Todo: create a node and then return it.
+	node := new(chord.ChordNode)
+	node.Initialize(GetLocalAddress() + ":" + fmt.Sprint(port))
+	return node
 }
-
-// Todo: implement a struct which implements the interface "dhtNode".
